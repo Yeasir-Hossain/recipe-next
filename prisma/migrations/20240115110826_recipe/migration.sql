35ecc31099f6e9bf5a38813c414c95ee8ea2bd14
Yeasir-Hossain <yeasir06@gmail.com>
2024-01-15 21:45:42 +0600

    fix:miigration file removed

--- prisma/migrations/20240115110826_recipe/migration.sql
+++
@@ -1,8 +0,0 @@
--- CreateTable
-CREATE TABLE "Recipe" (
-    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-    "title" TEXT NOT NULL,
-    "description" TEXT NOT NULL,
-    "ingredients" TEXT NOT NULL,
-    "resource" TEXT
-);
